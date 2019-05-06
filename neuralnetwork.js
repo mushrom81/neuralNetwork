23876e3e84de500aedb94c54f7e9a8c12697660a
mushrom81 <44351080+mushrom81@users.noreply.github.com>
2019-05-05 22:44:54 -0700

    1.5
    Now able to convert network into array and back

--- neuralnetwork.js
+++ neuralnetwork.js
@@ -1,119 +1,174 @@
-var c = document.querySelector("canvas");
+function sig(x) {
+    var denominator = 1 + Math.E ** -x;
+    return 1 / denominator;
+}
 
-c.width = 720;
-c.height = 480;
+function sizedArray(width, defaultValue = 0) {
+    var array = [];
+    for (var i = 0; i < width; i++) { array.push(defaultValue); }
+    return array;
+}
 
-var ctx = c.getContext("2d");
+function arrToStr(array) {
+    var string = "";
+    for (var i = 0; i < array.length; i++) {
+        string += array[i].toString();
+        if (i + 1 < array.length) string += ",";
+    }
+    return string;
+}
 
-function f(x) {
-    if (x > 0) return 1;
-    return 0;
+function largestIndex(array) {
+    var currentLargest = array[0];
+    var currentIndex = 0;
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] > currentLargest) {
+            currentLargest = array[i];
+            currentIndex = i;
+        }
+    }
+    return currentIndex;
+}
+
+function removeElementFromArray(array, index) {
+    var newArray = [];    
+    for (var i = 0; i < array.length; i++) {
+        if (i != index) newArray.push(array[i]);
+    }
+    return newArray;
 }
 
 class Node {
-    constructor(inputs, strength, layer, output, bias = 0) {
+    constructor(strengths, layer, bias, output = 0) {
+        this._strengths = strengths;
         this._layer = layer;
-        this._inputs = inputs;
-        this._output = output;
         this._bias = bias;
-        this._strength = strength;
+        this._output = output;
     }
 
-    get output() { return this._output; }
-    get inputs() { return this._inputs; }
+    get strengths() { return this._strengths; }
     get bias() { return this._bias; }
-    get strength() { return this._strength; }
+    set bias(bias) { this._bias = bias; }
+    get output() { return this._output; }
 
-    setOutput(manualinput = false) {
-        if (manualinput === false) {
+    mutateStrength(inputNode) {
+        var newStrength = Math.random() - 0.5;
+        this._strengths[inputNode] += newStrength;
+        if (this._strengths[inputNode] > 1) this._strengths[inputNode] = 1;
+        if (this._strengths[inputNode] < -1) this._strengths[inputNode] = -1;
+    }
+
+    setOutput(nestingNetwork) {
             var sum = this._bias;
-            for (var i = 0; i < this._inputs.length; i++) {
-                sum += brain.network[this._layer - 1][this._inputs[i]].output * this._strength[i];
+            for (var i = 0; i < this._strengths.length; i++) {
+                sum += nestingNetwork.layers[this._layer - 1][i].output * this._strengths[i];
             }
-            this._output = f(sum);
-        }
-        else this._output = f(manualinput);
+            this._output = sig(sum);
     }
 }
 
 class Network {
-    constructor() {
-        this._network = [];
+    constructor(widths) {
         this._layers = [];
+        this._widths = widths.slice();
+        this._outputs = [];
+        for (var i = 0; i < this._widths.length; i++) { this._layers.push([]); }
     }
 
-    get network() { return this._network; }
     get layers() { return this._layers; }
+    get widths() { return this._widths; }
+    get outputs() { return this._outputs; }
 
-    addNode(inputs, strength, layer, bias, output = 0) {
-        if (!this._network[layer]) {
-            this._network[layer] = [];
-            this._layers[layer] = 0;
+    createInputLayer(inputArray) {
+        this._layers[0] = [];
+        inputArray.forEach(bit => { this.addNode(false, 0, 0, bit); });
+    }
+    
+    createHiddenLayers() {
+        for (var layer = 1; layer < this._widths.length; layer++) {
+            for (var width = 0; width < this._widths[layer]; width++) {
+                this.addNode(sizedArray(this._widths[layer - 1]), layer, 0);
+            }
         }
-        this._network[layer].push(new Node(inputs, strength, layer, output, bias));
-        this._layers[layer]++;
+    }
+
+    clone() {
+        var clone = new Network(this._widths.slice());
+        for (var i = 1; i < this._widths.length; i++) {
+            for (var j = 0; j < this._widths[i]; j++) {
+                clone.addNode(this._layers[i][j].strengths.slice(), i, this._layers[i][j].bias);
+            }
+        }
+        return clone;
+    }
+
+    addNode(strengths, layer, bias, output = 0) {
+        this._layers[layer].push(new Node(strengths, layer, bias, output));
     }
 
     fireLayer(layer) {
-        for (var i = 0; i < this._network[layer].length; i++) {
-            this._network[layer][i].setOutput();
+        for (var i = 0; i < this._widths[layer]; i++) {
+            this._layers[layer][i].setOutput(this);
         }
     }
 
     runNetwork() {
-        for (var i = 1; i < this._network.length; i++) this.fireLayer(i);
+        for (var i = 1; i < this._widths.length; i++) this.fireLayer(i);
+        this._outputs = [];
+        for (var i = 0; i < this._widths[this._widths.length - 1]; i++) {
+            this._outputs.push(this._layers[this._widths.length - 1][i].output);
+        }
     }
-}
-brain = new Network();
 
-brain.addNode(false, false, 0, 0, -1);
-brain.addNode(false, false, 0, 0, -1);
-brain.addNode(false, false, 0, 0, 1);
-brain.addNode(false, false, 0, 0, 1);
-
-brain.addNode([0, 1], [-1, -1], 1, -1);
-brain.addNode([0, 1], [1, 1], 1, -1);
-brain.addNode([2, 3], [-1, -1], 1, -1);
-brain.addNode([2, 3], [1, 1], 1, -1);
-
-brain.addNode([0, 3], [1, 1], 2, -1);
-brain.addNode([1, 2], [1, 1], 2, -1);
-
-brain.addNode([0, 1], [1, 1], 3, 0);
-
-brain.runNetwork();
-
-function renderNetwork() {
-    for (var x = 0; x < brain.network.length; x++) {
-        for (var y = 0; y < brain.network[x].length; y++) {
-            if (brain.network[x][y].inputs !== false) {
-                for (var i = 0; i < brain.network[x][y].inputs.length; i++) {
-                    ctx.beginPath();
-                    ctx.moveTo(x * 40 + 15, y * 40 + 15);
-                    if (brain.network[x][y].strength[i] > 0) ctx.strokeStyle = "#FF0000"
-                    else ctx.strokeStyle = "#0000FF"
-                    ctx.lineWidth = Math.abs(brain.network[x][y].strength[i] * 4).toString();
-                    ctx.lineTo(x * 40 - 25, brain.network[x][y].inputs[i] * 40 + 15);
-                    ctx.stroke();
-                }              
+    exportNetwork() {
+        var exportedNetwork = [];
+        exportedNetwork.push(this._widths.length);
+        for (var i = 0; i < this._widths.length; i++) {
+            exportedNetwork.push(this._widths[i]);
+        }
+        for (var i = 1; i < this._layers.length; i++) {
+            for (var j = 0; j < this._layers[i].length; j++) {
+                exportedNetwork.push(this._layers[i][j].bias);
+                for (var k = 0; k < this._layers[i][j].strengths.length; k++) {
+                    exportedNetwork.push(this._layers[i][j].strengths[k]);
+                }
             }
-            if (brain.network[x][y].output >= 1) ctx.fillStyle = "#FF0000"
-            else ctx.fillStyle = "#0000FF"
-            ctx.fillRect(x * 40 + 10, y * 40 + 10, 10, 10);
-            if (brain.network[x][y].bias != 0) {
-                ctx.beginPath();
-                ctx.strokeStyle = "#000000"
-                ctx.lineWidth = "2"
-                ctx.rect(x * 40 + 10, y * 40 + 10, 10, 10);
-                ctx.stroke();
+        }
+        return exportedNetwork;
+    }
+
+    importNetwork(exportedNetwork) {
+        this._outputs = [];
+        this._widths = [];
+        this._layers = [];
+        var width = exportedNetwork.shift() + 1;
+        for (var i = 1; i < width; i++) {
+            this._widths.push(exportedNetwork.shift());
+            this._layers.push([]);
+        }
+        for (var i = 1; i < this._widths.length; i++) {
+            for (var j = 0; j < this._widths[i]; j++) {
+                var nodeBias = exportedNetwork.shift();
+                var nodeStrengths = [];
+                for (var k = 0; k < this._widths[i - 1]; k++) {
+                    nodeStrengths.push(exportedNetwork.shift());
+                }
+                this.addNode(nodeStrengths, i, nodeBias);
             }
         }
     }
 }
 
-function loop() {
-    requestAnimationFrame(loop);
-    ctx.clearRect(0, 0, c.width, c.height);
-    renderNetwork();
+function mutate(inputNetwork, mutationRate = 0.1) {
+    network = inputNetwork.clone();
+    for (var layer = 1; layer < network.widths.length; layer++) {
+        for (var width = 0; width < network.widths[layer]; width++) {
+            for (var connection = 0; connection < network.widths[layer - 1]; connection++) {
+                if (Math.random() < mutationRate) network.layers[layer][width].mutateStrength(connection);
+            }
+            if (Math.random() < mutationRate) network.layers[layer][width].bias += Math.random() - 0.5;
+        }
+    }
+    return network;
 }
-loop();
+
